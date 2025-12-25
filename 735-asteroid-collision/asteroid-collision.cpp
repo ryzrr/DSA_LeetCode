c61445595c609ed57f218746a8df43f9126c8cb9
Nikhil Kumar Rajak <145889151+ryzrr@users.noreply.github.com>
2025-12-25 21:24:21 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (87.25%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& arr) {
+        int n=arr.size();
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(arr[i]>0) ans.push_back(arr[i]);
+            else{
+                while(!ans.empty()&& ans.back()>0 && ans.back()<abs(arr[i])){
+                    ans.pop_back();
+                }
+                if(!ans.empty() && ans.back()==abs(arr[i])) ans.pop_back();
+                else if (ans.empty() || ans.back()<0){
+                    ans.push_back(arr[i]);
+                }
+            }
+        }
+        return ans;
+    }
+};
