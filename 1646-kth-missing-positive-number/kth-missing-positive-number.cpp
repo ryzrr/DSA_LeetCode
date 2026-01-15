c87f649afa8b1bd0a293a21e00afdb1062dfa2fe
Nikhil Kumar Rajak <145889151+ryzrr@users.noreply.github.com>
2026-01-15 22:30:38 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (90.45%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -5,10 +5,10 @@ public:
         int low =0 ,high=n-1;
         while(low<=high){
             int mid =(low+high)/2;
-            int missingNo = arr[mid]-(mid+1);
-            if(missingNo<k) low=mid+1;
+            int missingNumber = arr[mid]-(mid+1);
+            if(missingNumber<k) low=mid+1;
             else high=mid-1;
         }
-         return low+k; // or return high+1+K;  
+         return low+k; // or return high+1+K  both are same as explainby striver;  
     }
 };
