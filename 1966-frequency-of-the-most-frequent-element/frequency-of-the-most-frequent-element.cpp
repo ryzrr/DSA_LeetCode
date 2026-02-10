150a2eb56ace81e36f963133966baa8ccd5cab9c
Nikhil Kumar Rajak <145889151+ryzrr@users.noreply.github.com>
2026-02-10 22:58:14 +0530

    Time: 32 ms (85.83%) | Memory: 106 MB (11.00%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -4,7 +4,7 @@ public:
         sort(nums.begin(), nums.end());
 
         long long sum = 0;
-        int l = 0, ans = 1;
+        int l = 0, res = 1;
 
         for (int r = 0; r < nums.size(); r++) {
             sum += nums[r];
@@ -14,9 +14,9 @@ public:
                 l++;
             }
 
-            ans = max(ans, r - l + 1);
+            res = max(res, r - l + 1);
         }
 
-        return ans;
+        return res;
     }
 };
