bd0d0b404f44d29dafcd0e542f37960585f4bf49
Nikhil Kumar Rajak <145889151+ryzrr@users.noreply.github.com>
2026-02-14 01:17:05 +0530

    Time: 8 ms (15.11%) | Memory: 12.3 MB (16.08%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,47 +1,40 @@
 class Solution {
 public:
-    void solve(int row, int n, vector<string>& board, 
-               vector<int>& col,
-               vector<int>& leftDiag,
-               vector<int>& rightDiag,
-               vector<vector<string>>& ans) {
+    vector<vector<string>>ans;
+    unordered_set<int>cols;
+    unordered_set<int>diag;
+    unordered_set<int>antiDiag;
+    void solve(vector<string> &board,int row,int n){
         
-        if (row == n) {
+        if(row==n){
             ans.push_back(board);
-            return;
         }
-        
-        for (int c = 0; c < n; c++) {
-            
-            if (col[c] == 0 && 
-                leftDiag[row + c] == 0 && 
-                rightDiag[n - 1 + c - row] == 0) {
-                
-                board[row][c] = 'Q';
-                col[c] = 1;
-                leftDiag[row + c] = 1;
-                rightDiag[n - 1 + c - row] = 1;
-                
-                solve(row + 1, n, board, col, leftDiag, rightDiag, ans);
-                
-                board[row][c] = '.';
-                col[c] = 0;
-                leftDiag[row + c] = 0;
-                rightDiag[n - 1 + c - row] = 0;
+        for(int col=0;col<n;col++){
+            int diagConst = row+col;
+            int antiDiagConst= row-col;
+            if(cols.find(col)!=cols.end() ||diag.find(diagConst)!=diag.end()|| antiDiag.find(antiDiagConst)!=antiDiag.end()){
+                continue;
             }
+            cols.insert(col);
+            diag.insert(diagConst);
+            antiDiag.insert(antiDiagConst);
+            board[row][col]='Q';
+            solve(board,row+1,n);
+
+            cols.erase(col);
+            diag.erase(diagConst);
+            antiDiag.erase(antiDiagConst);
+            board[row][col]='.';
+
+
+
         }
     }
     
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
-        vector<string> board(n, string(n, '.'));
-        
-        vector<int> col(n, 0);
-        vector<int> leftDiag(2*n - 1, 0);
-        vector<int> rightDiag(2*n - 1, 0);
-        
-        solve(0, n, board, col, leftDiag, rightDiag, ans);
-        
+        vector<string>  board (n,string(n,'.'));
+        solve(board,0,n);
         return ans;
+         
     }
 };
