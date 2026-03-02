064ab17edbce05b9bc0c4167146aede7bef771c9
Nikhil Kumar Rajak <145889151+ryzrr@users.noreply.github.com>
2026-03-02 23:25:07 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (32.86%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int cnt =0;
+        for(int i =s.size()-1 ;i>=0;i--){
+            if(s[i]==' '){
+                if(cnt>0)break;
+                
+            }
+            else cnt++;
+           
+
+        }
+        return cnt ;
+    }
+};
