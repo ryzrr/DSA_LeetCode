848512346746625aafd4c61a1eea3b6b68f5e48a
Nikhil Kumar Rajak <145889151+ryzrr@users.noreply.github.com>
2026-06-21 17:38:06 +0530

    Time: 0 ms (100.00%) | Memory: 50.1 MB (77.70%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -8,24 +8,24 @@ public:
     int left = 0, right = n - 1;
 
     while (left <= right) {
-        int midCol = (left + right) / 2;
+        int midcolumn = (left + right) / 2;
 
-        // Find the max row in midCol
+        // Find the max row in midcolumn
         int maxRow = 0;
         for (int i = 1; i < m; ++i) {
-            if (mat[i][midCol] > mat[maxRow][midCol])
+            if (mat[i][midcolumn] > mat[maxRow][midcolumn])
                 maxRow = i;
         }
 
-        int leftVal = (midCol - 1 >= 0) ? mat[maxRow][midCol - 1] : -1;
-        int rightVal = (midCol + 1 < n) ? mat[maxRow][midCol + 1] : -1;
+        int leftVal = (midcolumn - 1 >= 0) ? mat[maxRow][midcolumn - 1] : -1;
+        int rightVal = (midcolumn + 1 < n) ? mat[maxRow][midcolumn + 1] : -1;
 
-        if (mat[maxRow][midCol] > leftVal && mat[maxRow][midCol] > rightVal) {
-            return {maxRow, midCol};
-        } else if (leftVal > mat[maxRow][midCol]) {
-            right = midCol - 1;
+        if (mat[maxRow][midcolumn] > leftVal && mat[maxRow][midcolumn] > rightVal) {
+            return {maxRow, midcolumn};
+        } else if (leftVal > mat[maxRow][midcolumn]) {
+            right = midcolumn - 1;
         } else {
-            left = midCol + 1;
+            left = midcolumn + 1;
         }
     }
 
