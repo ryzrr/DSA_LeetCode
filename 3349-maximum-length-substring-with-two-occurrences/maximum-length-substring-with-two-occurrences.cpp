12c73d9d6750a399d3c80f46ac249f4ceb7c8902
Nikhil Kumar Rajak <145889151+ryzrr@users.noreply.github.com>
2026-08-27 22:41:07 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (82.45%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int maximumLengthSubstring(string s) {
-        vector<int> arr(26,0);
+        vector<int> asstag(26,0);
         int  n = s.size();
         int left =0 ;
         int  ans = INT_MIN;
         for(int j =0 ;j<n;j++){
-            arr[s[j] - 'a']++;
-            while(arr[s[j]-'a']>2){
-                arr[s[left]-'a']--;
+            asstag[s[j] - 'a']++;
+            while(asstag[s[j]-'a']>2){
+                asstag[s[left]-'a']--;
                 left++;
             }
         
